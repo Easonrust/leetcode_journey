e725c019c4df37e30244f6bef75ddf397aef9827
Le Yang <36768174+Easonrust@users.noreply.github.com>
2021-07-31 19:14:28 +0800

    commit

--- test/src/App.java
+++ test/src/App.java
@@ -1,23 +1,40 @@
+import java.util.*;
+
 public class App {
-    public boolean judgeSquareSum(int c) {
-        int i = 0, j = (int) Math.sqrt(c);
-        while (i <= j) {
-            int sum = i * i + j * j;
-            if (sum == c) {
-                System.out.println(i);
-                System.out.println(j);
-                return true;
-            } else if (sum < c) {
-                i++;
-            } else {
-                j--;
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> frequencyForChar = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            frequencyForChar.put(s.charAt(i), frequencyForChar.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        List<Character>[] buckets = new ArrayList[s.length() + 1];
+        for (char key : frequencyForChar.keySet()) {
+            int frequency = frequencyForChar.get(key);
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>();
+            }
+            buckets[frequency].add(key);
+        }
+        ArrayList<Character> resultList = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (buckets[i] == null) {
+                continue;
+            }
+            for (int j = 0; j < buckets[i].size(); j++) {
+                for (int m = 0; m < i; m++) {
+                    resultList.add(buckets[i].get(j));
+                }
             }
         }
-        return false;
+        char[] resultArray = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+        String result = new String(resultArray);
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
         App app = new App();
-        System.out.println(app.judgeSquareSum(1000000));
+        System.out.println(app.frequencySort("tree"));
     }
 }
