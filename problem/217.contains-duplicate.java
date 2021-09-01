7e750adca5c2d281cecea047728b26aae8674ff4
Le Yang <36768174+Easonrust@users.noreply.github.com>
2021-09-01 19:51:38 +0800

    2021/09/01

---
+++ problem/217.contains-duplicate.java
@@ -0,0 +1,54 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=217 lang=java
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (57.80%)
+ * Likes:    2191
+ * Dislikes: 875
+ * Total Accepted:    926.9K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+ public boolean containsDuplicate(int[] nums) {
+  Set<Integer> set = new HashSet<>();
+  for (int num : nums) {
+   set.add(num);
+  }
+  return set.size() != nums.length;
+ }
+}
+// @lc code=end
+// 利用set中的元素是独一无二的特性
