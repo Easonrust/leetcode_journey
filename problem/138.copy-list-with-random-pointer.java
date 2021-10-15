d2e1951b933b9c455254b402b949c8fde8d160e2
Le Yang <36768174+Easonrust@users.noreply.github.com>
2021-10-14 20:48:57 -0700

    2021/10/14

---
+++ problem/138.copy-list-with-random-pointer.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+ Map<Node, Node> map = new HashMap<>();
+
+ public Node copyRandomList(Node head) {
+  if (head == null) {
+   return null;
+  }
+
+  if (map.containsKey(head)) {
+   return map.get(head);
+  }
+
+  Node node = new Node(head.val);
+
+  map.put(head, node);
+  node.next = copyRandomList(head.next);
+  node.random = copyRandomList(head.random);
+
+  return node;
+ }
+}
+
+// 把已经copy过的节点用一个哈希表存起来
+// @lc code=end
