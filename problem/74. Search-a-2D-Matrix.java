b3eebd06b1125ad9d8d407ba534bc49189744186
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-03-31 19:30:39 -0700

    2022/03/30

---
+++ problem/74. Search-a-2D-Matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0;
+        int h = m * n - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            int x = mid / n;
+            int y = mid % n;
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+}
+
+// binary search
