a1e5e688fa52ba6517b4a8ec5072da611c0ddef9
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-06-30 21:28:17 -0700

    2022/06/30

--- problem/64.minimum-path-sum.java
+++ problem/64.minimum-path-sum.java
@@ -1,24 +1,23 @@
-// @lc code=start
 class Solution {
- public int minPathSum(int[][] grid) {
-  int m = grid.length;
-  int n = grid[0].length;
-  // int left = 0, top = 0, cur = 0;
-  // 因为要存中间的值以及对应的位置，所以不能这么写
-  int[] dp = new int[n];
-  for (int i = 0; i < m; ++i) {
-   for (int j = 0; j < n; ++j) {
-    if (j == 0) {
-     dp[j] = dp[j];
-    } else if (i == 0) {
-     dp[j] = dp[j - 1];
-    } else {
-     dp[j] = Math.min(dp[j], dp[j - 1]);
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<m;++i){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        
+        for(int j=1; j<n;++j){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        
+        for(int i=1; i<m; ++i){
+            for(int j=1; j<n; ++j){
+                dp[i][j] = grid[i][j] + Math.min(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
-    dp[j] += grid[i][j];
-   }
-  }
-  return dp[n - 1];
- }
 }
-// @lc code=end
