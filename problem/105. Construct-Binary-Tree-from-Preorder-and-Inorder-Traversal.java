9eab98b0ae46c3073eabf3555d2cfe0645a98e66
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-07-13 21:49:34 -0700

    2022/07/13

--- problem/105. Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.java
+++ problem/105. Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.java
@@ -1,41 +1,40 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    Map<Integer, Integer> inIdxForVal;
-
+    Map<Integer, Integer> idxForVal;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        inIdxForVal = new HashMap<>();
-        for (int i = 0; i < inorder.length; ++i) {
-            inIdxForVal.put(inorder[i], i);
+        idxForVal = new HashMap<>();
+        for(int i=0; i<inorder.length; ++i){
+            idxForVal.put(inorder[i], i);
         }
-        return helper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        TreeNode root = helper(preorder, inorder, 0, preorder.length-1, 0, inorder.length-1);
+        return root;
     }
-
-    private TreeNode helper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
-        if (preStart > preEnd) {
+    
+    private TreeNode helper(int[] preorder, int[] inorder, int prestart, int preend, int instart, int inend){
+        if(prestart>preend){
             return null;
         }
-        int rootVal = preorder[preStart];
-        int rootInIdx = inIdxForVal.get(rootVal);
-        TreeNode root = new TreeNode(rootVal);
-
-        int leftSize = rootInIdx - 1 - inStart;
-
-        root.left = helper(preorder, inorder, preStart + 1, preStart + 1 + leftSize, inStart, rootInIdx - 1);
-        root.right = helper(preorder, inorder, preStart + 1 + leftSize + 1, preEnd, rootInIdx + 1, inEnd);
+        int rootval = preorder[prestart];
+        TreeNode root = new TreeNode(rootval);
+        int idx = idxForVal.get(rootval);
+        
+        int leftsize = idx - instart;
+        root.left = helper(preorder, inorder, prestart+1, prestart+leftsize, instart, idx-1);
+        root.right = helper(preorder, inorder, prestart+leftsize+1, preend, idx+1, inend);
         return root;
     }
 }
