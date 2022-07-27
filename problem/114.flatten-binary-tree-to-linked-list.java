50e4a8b182290bd6c363286d0f0fb8f99a9e96b0
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-07-27 10:16:48 -0700

    2022/07/26

--- problem/114.flatten-binary-tree-to-linked-list.java
+++ problem/114.flatten-binary-tree-to-linked-list.java
@@ -1,47 +1,38 @@
-/*
- * @lc app=leetcode id=114 lang=java
- *
- * [114] Flatten Binary Tree to Linked List
- */
-
-// @lc code=start
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
- public void flatten(TreeNode root) {
-  root = preOrder(root);
- }
-
- private TreeNode preOrder(TreeNode root) {
-  if (root == null) {
-   return null;
-  }
-  TreeNode node = root;
-  TreeNode left = root.left;
-  TreeNode right = root.right;
-  node.left = null;
-  node.right = preOrder(left);
-  TreeNode temp = node;
-  while (temp.right != null) {
-   temp = temp.right;
-  }
-  temp.right = preOrder(right);
-
-  return node;
-
- }
+    public void flatten(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        
+        flatten(root.left);
+        flatten(root.right);
+        
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        root.left = null;
+        root.right = left;
+        
+        TreeNode node = root;
+        while(node.right!=null){
+            node = node.right;
+        }
+        
+        node.right = right;
+    }
 }
-// @lc code=end
