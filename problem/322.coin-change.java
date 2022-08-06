57f057ee413d928a5117f8db595fb63dcc00006e
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-08-05 21:02:29 -0700

    2022/08/05

--- problem/322.coin-change.java
+++ problem/322.coin-change.java
@@ -1,39 +1,20 @@
 class Solution {
-    int[] memo;
     public int coinChange(int[] coins, int amount) {
-        memo = new int[amount+1];
-        Arrays.fill(memo, -2);
-        return dp(coins, amount);
-    }
-    
-    public int dp(int[] coins, int amount){
-        if(amount==0){
-            return 0;
-        }
-        
-        if(amount<0){
-            return -1;
-        }
-        
-        if(memo[amount]!=-2){
-            return memo[amount];
-        }
-        
-        int res = Integer.MAX_VALUE;
-        for(int coin:coins){
-            int sub = dp(coins, amount-coin);
-            if(sub!=-1){
-                res = Math.min(res, sub+1);
+        int[] dp = new int[amount + 1];
+        // 数组大小为 amount + 1，初始值也为 amount + 1
+        Arrays.fill(dp, amount + 1);
+
+        // base case
+        dp[0] = 0;
+        // 外层 for 循环在遍历所有状态的所有取值
+        for(int j = 0; j<coins.length; ++j) {
+            for (int i = 0; i < dp.length; i++) {
+                if (i - coins[j] < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
             }
         }
-        
-        if(res==Integer.MAX_VALUE){
-            memo[amount] = -1;
-            return -1;
-        }
-        
-        memo[amount] = res;
-        
-        return res;
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
