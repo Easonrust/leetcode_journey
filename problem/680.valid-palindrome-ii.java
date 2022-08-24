292649147374336562f4c41eeb2b9a66d39db83e
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-08-23 20:26:06 -0700

    2022/08/23

--- problem/680.valid-palindrome-ii.java
+++ problem/680.valid-palindrome-ii.java
@@ -1,77 +1,29 @@
-/*
- * @lc app=leetcode id=680 lang=java
- *
- * [680] Valid Palindrome II
- *
- * https://leetcode.com/problems/valid-palindrome-ii/description/
- *
- * algorithms
- * Easy (37.46%)
- * Likes:    2944
- * Dislikes: 186
- * Total Accepted:    294.1K
- * Total Submissions: 784.8K
- * Testcase Example:  '"aba"'
- *
- * Given a string s, return true if the s can be palindrome after deleting at
- * most one character from it.
- * 
- * 
- * Example 1:
- * 
- * 
- * Input: s = "aba"
- * Output: true
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: s = "abca"
- * Output: true
- * Explanation: You could delete the character 'c'.
- * 
- * 
- * Example 3:
- * 
- * 
- * Input: s = "abc"
- * Output: false
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * 1 <= s.length <= 10^5
- * s consists of lowercase English letters.
- * 
- * 
- */
-
-// @lc code=start
 class Solution {
- public boolean validPalindrome(String s) {
-  int i = 0, j = s.length() - 1;
-  while (i < j) {
-
-   // 这里注意判断里面中要用到i和j的值，因此不能这样简写
-   if (s.charAt(i) != s.charAt(j)) {
-    return checkPalindrome(i + 1, j, s) || checkPalindrome(i, j - 1, s);
-   }
-   i++;
-   j--;
-  }
-  return true;
- }
-
- public boolean checkPalindrome(int i, int j, String s) {
-  while (i < j) {
-   if (s.charAt(i++) != s.charAt(j--)) {
-    return false;
-   }
-  }
-  return true;
- }
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            char ch1 = s.charAt(l);
+            char ch2 = s.charAt(r);
+            if(ch1!=ch2){
+                return checkPalindrome(l+1, r, s) || checkPalindrome(l, r-1, s);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    private boolean checkPalindrome(int l, int r, String s){
+        while(l<r){
+            char ch1 = s.charAt(l);
+            char ch2 = s.charAt(r);
+            if(ch1!=ch2){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
 }
-// @lc code=end
--- problem/953.verifying-an-alien-dictionary.java
+++ problem/953.verifying-an-alien-dictionary.java
@@ -1,36 +1,26 @@
-/*
- * @lc app=leetcode id=953 lang=java
- *
- * [953] Verifying an Alien Dictionary
- */
-
-// @lc code=start
 class Solution {
- public boolean isAlienSorted(String[] words, String order) {
-  for (int i = 0; i < words.length - 1; ++i) {
-   String word1 = words[i];
-   String word2 = words[i + 1];
-
-   if (word1.startsWith(word2) && word1.length() > word2.length()) {
-    return false;
-   }
-
-   for (int j = 0; j < Math.min(word1.length(), word2.length()); ++j) {
-    if (word1.charAt(j) == word2.charAt(j)) {
-     continue;
+    public boolean isAlienSorted(String[] words, String order) {
+        for(int i=0; i<words.length-1; ++i){
+            String word1 = words[i];
+            String word2 = words[i+1];
+            if(word1.startsWith(word2)&&word1.length()>word2.length()){
+                return false;
+            }
+            
+            for(int j=0; j<Math.min(word1.length(), word2.length()); ++j){
+                char ch1 = word1.charAt(j);
+                char ch2 = word2.charAt(j);
+                if(ch1==ch2){
+                    continue;
+                }
+                if(order.indexOf(ch1)>order.indexOf(ch2)){
+                    return false;
+                }else{
+                    break;
+                }
+            }
+        }
+        
+        return true;
     }
-
-    if (order.indexOf(word1.charAt(j)) > order.indexOf(word2.charAt(j))) {
-     return false;
-    } else {
-     break;
-    }
-   }
-  }
-
-  return true;
- }
 }
-
-// 269 的基础
-// @lc code=end
