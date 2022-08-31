a5e9dd7d4b948d60c4bb1533ad106d7192bfb9b0
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-08-30 20:54:10 -0700

    2022/08/30

--- problem/36.valid-sudoku.java
+++ problem/36.valid-sudoku.java
@@ -1,43 +1,24 @@
-// @lc code=start
 class Solution {
- public boolean isValidSudoku(char[][] board) {
-     Set<Character>[] rows=new HashSet[9];
-     Set<Character>[] cols=new HashSet[9];
-     Set<Character>[] boxes=new HashSet[9];
-     
-     
-     for(int i=0;i<9;++i){
-         rows[i]=new HashSet<>();
-         cols[i]=new HashSet<>();
-         boxes[i]=new HashSet<>();
-     }
-     
-     for(int i=0;i<9;++i){
-         for(int j=0;j<9;++j){
-             if(Character.isDigit(board[i][j])){
-                 if(rows[i].contains(board[i][j])){
-                     return false;
-                 }else{
-                     rows[i].add(board[i][j]);
-                 }
-                 if(cols[j].contains(board[i][j])){
-                     return false;
-                 }else{
-                     cols[j].add(board[i][j]);
-                 }
-
-                 if(boxes[i/3*3+j/3].contains(board[i][j])){
-                     return false;
-                 }else{
-                     boxes[i/3*3+j/3].add(board[i][j]);
-                 }
-             }
-         }
-     }
-     return true;
- }
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] cols = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        
+        for(int i=0; i<9; ++i){
+            for(int j=0; j<9; ++j){
+                char ch = board[i][j];
+                if(ch!='.'){
+                    int digit = ch - '0' - 1;
+                    rows[i][digit]++;
+                    cols[j][digit]++;
+                    subboxes[i/3][j/3][digit]++;
+                    if(rows[i][digit]>1||cols[j][digit]>1||subboxes[i/3][j/3][digit]>1){
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
 }
-
-// HashSet 在每一个格子处进行判断即可
-// @lc code=end
-
