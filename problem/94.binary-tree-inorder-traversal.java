3b79d6b1ccb5c5251ba46f93a1d3bc548f7d6ad3
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-09-07 20:44:13 -0700

    2022/09/07

--- problem/94.binary-tree-inorder-traversal.java
+++ problem/94.binary-tree-inorder-traversal.java
@@ -1,101 +1,32 @@
-/*
- * @lc app=leetcode id=94 lang=java
- *
- * [94] Binary Tree Inorder Traversal
- *
- * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
- *
- * algorithms
- * Easy (67.59%)
- * Likes:    5499
- * Dislikes: 235
- * Total Accepted:    1.1M
- * Total Submissions: 1.6M
- * Testcase Example:  '[1,null,2,3]'
- *
- * Given the root of a binary tree, return the inorder traversal of its nodes'
- * values.
- * 
- * 
- * Example 1:
- * 
- * 
- * Input: root = [1,null,2,3]
- * Output: [1,3,2]
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: root = []
- * Output: []
- * 
- * 
- * Example 3:
- * 
- * 
- * Input: root = [1]
- * Output: [1]
- * 
- * 
- * Example 4:
- * 
- * 
- * Input: root = [1,2]
- * Output: [2,1]
- * 
- * 
- * Example 5:
- * 
- * 
- * Input: root = [1,null,2]
- * Output: [1,2]
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * The number of nodes in the tree is in the range [0, 100].
- * -100 <= Node.val <= 100
- * 
- * 
- * 
- * Follow up: Recursive solution is trivial, could you do it iteratively?
- */
-
-// @lc code=start
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
- * = left; this.right = right; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
- // 递归写法
- private List<Integer> ret;
-
- public List<Integer> inorderTraversal(TreeNode root) {
-  ret = new ArrayList<>();
-  if (root == null) {
-   return ret;
-  }
-  dfs(root);
-  return ret;
- }
-
- private void dfs(TreeNode root) {
-  if (root == null) {
-   return;
-  }
-  dfs(root.left);
-  ret.add(root.val);
-  dfs(root.right);
- }
-
- // 非递归写法待完成
- // public List<Integer> inorderTraversal(TreeNode root) {
- // }
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inOrder(root);
+        return res;
+    }
+    
+    private void inOrder(TreeNode root) {
+        if(root==null) {
+            return;   
+        }
+        
+        inOrder(root.left);
+        res.add(root.val);
+        inOrder(root.right);
+    }
 }
-// @lc code=end
-// 递归写法比较简单
