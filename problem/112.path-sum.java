84bde9e67d1ae4182e34a045c12439179c575edb
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-10-03 19:56:48 -0700

    2022/10/03

--- problem/112.path-sum.java
+++ problem/112.path-sum.java
@@ -1,76 +1,44 @@
-/*
- * @lc app=leetcode id=112 lang=java
- *
- * [112] Path Sum
- *
- * https://leetcode.com/problems/path-sum/description/
- *
- * algorithms
- * Easy (43.35%)
- * Likes:    3787
- * Dislikes: 668
- * Total Accepted:    682.4K
- * Total Submissions: 1.6M
- * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
- *
- * Given the root of a binary tree and an integer targetSum, return true if the
- * tree has a root-to-leaf path such that adding up all the values along the
- * path equals targetSum.
- * 
- * A leaf is a node with no children.
- * 
- * 
- * Example 1:
- * 
- * 
- * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
- * Output: true
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: root = [1,2,3], targetSum = 5
- * Output: false
- * 
- * 
- * Example 3:
- * 
- * 
- * Input: root = [1,2], targetSum = 0
- * Output: false
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * The number of nodes in the tree is in the range [0, 5000].
- * -1000 <= Node.val <= 1000
- * -1000 <= targetSum <= 1000
- * 
- * 
- */
-
-// @lc code=start
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
- * = left; this.right = right; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
  */
 class Solution {
- public boolean hasPathSum(TreeNode root, int targetSum) {
-  if (root == null) {
-   return false;
-  }
+    boolean res = false;
 
-  // the last leaf node
-  if (root.left == null && root.right == null && root.val == targetSum) {
-   return true;
-  }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return res;
+        }
+        dfs(root, targetSum);
+        return res;
+    }
 
-  return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
- }
+    private void dfs(TreeNode root, int targetSum) {
+        if (res == true) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            if (root.val == targetSum) {
+                res = true;
+            }
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left, targetSum - root.val);
+        }
+        if (root.right != null) {
+            dfs(root.right, targetSum - root.val);
+        }
+    }
 }
-// @lc code=end
