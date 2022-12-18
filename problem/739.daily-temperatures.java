0e19940322ae92235ccf8ef3775b099b7a6b419b
Le Yang <36768174+Easonrust@users.noreply.github.com>
2022-12-17 21:53:21 -0800

    2022/12/17

--- problem/739.daily-temperatures.java
+++ problem/739.daily-temperatures.java
@@ -3,16 +3,13 @@ class Solution {
         int n = temperatures.length;
         int[] res = new int[n];
         Deque<Integer> stack = new LinkedList<>();
-        for(int i=n-1;i>=0;i--){
-            while(!stack.isEmpty()&&temperatures[stack.peekLast()]<=temperatures[i]){
+        for(int i=n-1; i>=0; i--){
+            while(!stack.isEmpty()&&temperatures[stack.getLast()]<=temperatures[i]){
                 stack.removeLast();
             }
-            res[i] = stack.isEmpty() ? 0 : stack.getLast()-i;
+            res[i] = stack.isEmpty() ? 0 : (stack.getLast() - i);
             stack.addLast(i);
         }
-        
         return res;
     }
 }
-
-// Monotonic Stack
