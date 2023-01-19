eed2e2e989da4632e2a57a0eac01ee839e477750
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-01-19 15:36:39 -0800

    2023/01/19

--- problem/974. Subarray Sums Divisible by K.java
+++ problem/974. Subarray Sums Divisible by K.java
@@ -1,22 +1,23 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int[] preSum = new int[nums.length+1];
+        int n = nums.length;
+        int[] preSum = new int[n+1];
         preSum[0] = 0;
-        for(int i=1; i<=nums.length; ++i){
+        for(int i=1; i<=n; ++i){
             preSum[i] = preSum[i-1] + nums[i-1];
         }
-        
-        int res = 0;
+
         Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<preSum.length; ++i){
+        int res = 0;
+        for(int i=0; i<=n; ++i){
             int mod = Math.floorMod(preSum[i], k);
             if(map.containsKey(mod)){
                 res += map.get(mod);
             }
-            map.put(mod, map.getOrDefault(mod,0)+1);
+            map.put(mod, map.getOrDefault(mod, 0)+1);
         }
-        
         return res;
     }
 }
+
+// 同余定理
