5f1f2d4cb5736385627619cf1e5ce3ba148e7ed1
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-02-03 16:59:43 -0800

    2022/02/03

--- problem/567. Permutation in String.java
+++ problem/567. Permutation in String.java
@@ -2,36 +2,29 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] window = new int[26];
         int[] needs = new int[26];
-
-        for (char c : s1.toCharArray()) {
-            needs[c - 'a']++;
+        for(char ch1:s1.toCharArray()){
+            needs[ch1-'a']++;
         }
-
         int left = 0;
         int right = 0;
-        List<Integer> res = new ArrayList<>();
-        while (right < s2.length()) {
-            char c1 = s2.charAt(right);
+        while(right<s2.length()){
+            char in = s2.charAt(right);
             right++;
-
-            window[c1 - 'a']++;
-
-            while (right - left > s1.length()) {
-                char c2 = s2.charAt(left);
+            window[in-'a']++;
+            while(right-left>s1.length()){
+                char out = s2.charAt(left);
                 left++;
-
-                window[c2 - 'a']--;
+                window[out-'a']--;
             }
-
-            if (right - left == s1.length()) {
+            if(right-left==s1.length()){
                 boolean flag = true;
-                for (int i = 0; i < window.length; ++i) {
-                    if (window[i] != needs[i]) {
+                for(int i=0; i<26; ++i){
+                    if(window[i]!=needs[i]){
                         flag = false;
                         break;
                     }
                 }
-                if (flag) {
+                if(flag==true){
                     return true;
                 }
             }
