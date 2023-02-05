1dd1c96d208d924c3afaf7928996ad9e42204a96
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-02-05 15:40:37 -0800

    2023/02/05

--- problem/438.find-all-anagrams-in-a-string.java
+++ problem/438.find-all-anagrams-in-a-string.java
@@ -2,24 +2,21 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         int[] window = new int[26];
         int[] needs = new int[26];
-        for(char c:p.toCharArray()){
-            needs[c-'a']++;
+        for(char ch:p.toCharArray()){
+            needs[ch-'a']++;
         }
         int left = 0;
         int right = 0;
-        int n = s.length();
         List<Integer> res = new ArrayList<>();
-        while(right<n){
+        while(right<s.length()){
             char in = s.charAt(right);
-            right++;
             window[in-'a']++;
-
+            right++;
             while(right-left>p.length()){
                 char out = s.charAt(left);
-                left++;
                 window[out-'a']--;
+                left++;
             }
-
             if(right-left==p.length()){
                 boolean match = true;
                 for(int i=0; i<26; ++i){
