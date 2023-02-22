2977d38ec5bc4691307e1f45f64368fe107cbcf3
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-02-21 19:40:22 -0800

    2023/02/21

--- problem/1011. Capacity-To-Ship-Packages-Within-D-Days.java
+++ problem/1011. Capacity-To-Ship-Packages-Within-D-Days.java
@@ -2,40 +2,33 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int l = 0;
         int r = 0;
-        for (int weight : weights) {
-            r += weight;
+        for(int weight:weights){
             l = Math.max(l, weight);
+            r += weight;
         }
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (getTime(weights, mid) > days) {
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(helper(weights, mid)<=days){
+                r = mid;
+            }else{
                 l = mid + 1;
-            } else {
-                r = mid - 1;
             }
         }
-
-        return l;
+        return r;
     }
 
-    private int getTime(int[] weights, int k) {
-        int t = 0;
+    private int helper(int[] weights, int cap) {
+        int res = 1;
         int i = 0;
-
         int sum = 0;
-        while (i < weights.length) {
+        while(i<weights.length){
             sum += weights[i];
-            if (sum > k) {
-                t++;
-                sum = 0;
-            } else if (i == weights.length - 1) {
-                t++;
-                i++;
-            } else {
-                i++;
+            if(sum>cap){
+                sum = weights[i];
+                res++;
             }
+            i++;
         }
-        return t;
+        return res;
     }
 }
--- problem/540.single-element-in-a-sorted-array.java
+++ problem/540.single-element-in-a-sorted-array.java
@@ -2,22 +2,17 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int l = 0;
         int r = nums.length - 1;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (mid % 2 != 0) {
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(mid%2!=0){
                 mid--;
             }
-
-            if (nums[mid] == nums[mid + 1]) {
+            if(nums[mid]==nums[mid+1]){
                 l = mid + 2;
-            } else {
+            }else{
                 r = mid;
             }
         }
-
         return nums[r];
     }
 }
-// @lc code=end
-// 单元素一定在偶数位上,因题目的复杂度要求，使用二分查找法进行求解
-// 只要出现h=m就要写l<h
