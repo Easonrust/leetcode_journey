356f28f3d57c9a4935c0502442f6ab9d08cf6cdf
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-03-13 15:33:21 -0700

    2023/03/13

--- problem/101.symmetric-tree.java
+++ problem/101.symmetric-tree.java
@@ -1,77 +1,36 @@
-import javax.swing.tree.TreeNode;
-
-/*
- * @lc app=leetcode id=101 lang=java
- *
- * [101] Symmetric Tree
- *
- * https://leetcode.com/problems/symmetric-tree/description/
- *
- * algorithms
- * Easy (49.38%)
- * Likes:    7071
- * Dislikes: 183
- * Total Accepted:    978.8K
- * Total Submissions: 2M
- * Testcase Example:  '[1,2,2,3,4,4,3]'
- *
- * Given the root of a binary tree, check whether it is a mirror of itself
- * (i.e., symmetric around its center).
- * 
- * 
- * Example 1:
- * 
- * 
- * Input: root = [1,2,2,3,4,4,3]
- * Output: true
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: root = [1,2,2,null,3,null,3]
- * Output: false
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * The number of nodes in the tree is in the range [1, 1000].
- * -100 <= Node.val <= 100
- * 
- * 
- * 
- * Follow up: Could you solve it both recursively and iteratively?
- */
-
-// @lc code=start
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
- * = left; this.right = right; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
- public boolean isSymmetric(TreeNode root) {
-  if (root == null) {
-   return false;
-  }
-  return isSymmetricAsRoot(root.left, root.right);
- }
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
 
- public boolean isSymmetricAsRoot(TreeNode t1, TreeNode t2) {
-  if (t1 == null && t2 == null) {
-   return true;
-  } else if (t1 == null || t2 == null) {
-   return false;
-  }
-  if (t1.val != t2.val) {
-   return false;
-  }
-  return isSymmetricAsRoot(t1.left, t2.right) && isSymmetricAsRoot(t1.right, t2.left);
+    public boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if(root1==null&&root2==null){
+            return true;
+        }
 
- }
+        if(root1==null||root2==null){
+            return false;
+        }
+
+        if(root1.val!=root2.val){
+            return false;
+        }
+
+        return isSymmetric(root1.left, root2.right)&&isSymmetric(root1.right, root2.left);
+    }
 }
-// @lc code=end
-// 和子树包含问题的树比较方式相似
