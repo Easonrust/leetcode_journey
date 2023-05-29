03bbd378bcc4f633025cb1e8be7ec172690135c2
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-05-29 14:25:37 -0700

    2023/05/29

--- problem/1603.design-parking-system.java
+++ problem/1603.design-parking-system.java
@@ -1,40 +1,23 @@
-/*
- * @lc app=leetcode id=1603 lang=java
- *
- * [1603] Design Parking System
- */
-
-// @lc code=start
 class ParkingSystem {
- Map<Integer, Integer> map;
-
- public ParkingSystem(int big, int medium, int small) {
-  map = new HashMap<>();
-  map.put(1, big);
-  map.put(2, medium);
-  map.put(3, small);
- }
-
- public boolean addCar(int carType) {
-  if (map.get(carType) == 0) {
-   return false;
-  } else {
-   map.put(carType, map.get(carType) - 1);
-   return true;
-  }
- }
+    int[] space;
+    public ParkingSystem(int big, int medium, int small) {
+        space = new int[]{big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        if(space[carType-1]==0){
+            return false;
+        }else{
+            space[carType-1]--;
+            return true;
+        }
+    }
 }
 
 /**
  * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem obj = new ParkingSystem(big, medium, small); boolean param_1 =
- * obj.addCar(carType);
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
  */
 
 // too easy
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem obj = new ParkingSystem(big, medium, small); boolean param_1 =
- * obj.addCar(carType);
- */
-// @lc code=end
