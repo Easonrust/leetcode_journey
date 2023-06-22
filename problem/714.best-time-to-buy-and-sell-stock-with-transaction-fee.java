94243192f089c995b80886891d5ed1fd1b29e200
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-06-22 14:40:22 -0700

    2023/06/22

--- problem/714.best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ problem/714.best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,77 +1,16 @@
-/*
- * @lc app=leetcode id=714 lang=java
- *
- * [714] Best Time to Buy and Sell Stock with Transaction Fee
- *
- * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
- *
- * algorithms
- * Medium (58.89%)
- * Likes:    2882
- * Dislikes: 80
- * Total Accepted:    120.6K
- * Total Submissions: 204.2K
- * Testcase Example:  '[1,3,2,8,4,9]\n2'
- *
- * You are given an array prices where prices[i] is the price of a given stock
- * on the i^th day, and an integer fee representing a transaction fee.
- * 
- * Find the maximum profit you can achieve. You may complete as many
- * transactions as you like, but you need to pay the transaction fee for each
- * transaction.
- * 
- * Note: You may not engage in multiple transactions simultaneously (i.e., you
- * must sell the stock before you buy again).
- * 
- * 
- * Example 1:
- * 
- * 
- * Input: prices = [1,3,2,8,4,9], fee = 2
- * Output: 8
- * Explanation: The maximum profit can be achieved by:
- * - Buying at prices[0] = 1
- * - Selling at prices[3] = 8
- * - Buying at prices[4] = 4
- * - Selling at prices[5] = 9
- * The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: prices = [1,3,7,5,10,3], fee = 3
- * Output: 6
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * 1 <= prices.length <= 5 * 10^4
- * 1 <= prices[i] < 5 * 10^4
- * 0 <= fee < 5 * 10^4
- * 
- * 
- */
-
-// @lc code=start
 class Solution {
- public int maxProfit(int[] prices, int fee) {
-  int n = prices.length;
-  int[] buy = new int[n];
-  int[] sell = new int[n];
-  int[] c1 = new int[n];
-  int[] c2 = new int[n];
-  buy[0] = c1[0] = -prices[0];
-  for (int i = 1; i < n; ++i) {
-   buy[i] = Math.max(sell[i - 1] - prices[i], c2[i - 1] - prices[i]);
-   sell[i] = Math.max(buy[i - 1] + prices[i] - fee, c1[i - 1] + prices[i] - fee);
-   c1[i] = Math.max(buy[i - 1], c1[i - 1]);
-   c2[i] = Math.max(sell[i - 1], c2[i - 1]);
-  }
-  return Math.max(c2[n - 1], sell[n - 1]);
- }
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for(int i=0; i<n; ++i){
+            if(i==0){
+                dp[i][0] = 0;
+                dp[i][1] = (-prices[i]-fee);
+            }else{
+                dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]);
+                dp[i][1] = Math.max(dp[i-1][0]-prices[i]-fee, dp[i-1][1]);
+            }
+        }
+        return Math.max(dp[n-1][0], dp[n-1][1]);
+    }
 }
-// @lc code=end
-// 和309一样，注意总是可以cooldown的
