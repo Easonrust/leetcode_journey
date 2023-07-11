5c74a8ac7112c7e1ac56b8e04d79a512193297a2
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-07-11 15:23:39 -0700

    2023/07/11

--- problem/863.all-nodes-distance-k-in-binary-tree.java
+++ problem/863.all-nodes-distance-k-in-binary-tree.java
@@ -1,66 +1,47 @@
-/*
- * @lc app=leetcode id=863 lang=java
- *
- * [863] All Nodes Distance K in Binary Tree
- */
-
-// @lc code=start
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 class Solution {
- List<Integer> ans;
- TreeNode target;
- int K;
+    Map<Integer, TreeNode> parents = new HashMap<>();
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        findParents(root);
+        findNodes(target, new TreeNode(-1), 0, k);
+        return ans;
+    }
 
- public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-  ans = new LinkedList();
-  this.target = target;
-  this.K = K;
-  dfs(root);
-  return ans;
- }
+    private void findParents(TreeNode node) {
+        if(node.left!=null){
+            parents.put(node.left.val, node);
+            findParents(node.left);
+        }
+        if(node.right!=null){
+            parents.put(node.right.val, node);
+            findParents(node.right);
+        }
+    }
 
- // Return vertex distance from node to target if exists, else -1
- // Vertex distance: the number of vertices on the path from node to target
- public int dfs(TreeNode node) {
-  if (node == null)
-   return -1;
-  else if (node == target) {
-   subtree_add(node, 0);
-   return 1;
-  } else {
-   int L = dfs(node.left), R = dfs(node.right);
-   if (L != -1) {
-    if (L == K)
-     ans.add(node.val);
-    subtree_add(node.right, L + 1);
-    return L + 1;
-   } else if (R != -1) {
-    if (R == K)
-     ans.add(node.val);
-    subtree_add(node.left, R + 1);
-    return R + 1;
-   } else {
-    return -1;
-   }
-  }
- }
+    private void findNodes(TreeNode node, TreeNode from, int depth, int k) {
+        if(depth==k){
+            ans.add(node.val);
+        }
 
- // Add all nodes 'K - dist' from the node to answer.
- public void subtree_add(TreeNode node, int dist) {
-  if (node == null)
-   return;
-  if (dist == K)
-   ans.add(node.val);
-  else {
-   subtree_add(node.left, dist + 1);
-   subtree_add(node.right, dist + 1);
-  }
- }
+        if(node.left!=null&&node.left.val!=from.val){
+            findNodes(node.left, node, depth+1, k);
+        }
+
+        if(node.right!=null&&node.right.val!=from.val){
+            findNodes(node.right, node, depth+1, k);
+        }
+
+        if(parents.containsKey(node.val)&&parents.get(node.val).val!=from.val){
+            findNodes(parents.get(node.val), node, depth+1, k);
+        }
+    }
 }
-// @lc code=end
-// From root, say the target node is at depth 3 in the left branch. It means
-// that any nodes that are distance K - 3 in the right branch should be added to
-// the answer.
