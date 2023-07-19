f62d3e04e01ee5c4ad65656f9232c4e5c03e9f80
Le Yang <36768174+Easonrust@users.noreply.github.com>
2023-07-19 15:13:13 -0700

    2023/07/19

--- problem/435.non-overlapping-intervals.java
+++ problem/435.non-overlapping-intervals.java
@@ -1,31 +1,17 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-/*
- * @lc app=leetcode id=435 lang=java
- *
- * [435] Non-overlapping Intervals
- *
- * https://leetcode.com/problems/non-overlapping-intervals/description/
- */
-
-// @lc code=start
 class Solution {
- public int eraseOverlapIntervals(int[][] intervals) {
-  Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
-  int cnt = 1;
-  int end = intervals[0][1];
-  for (int i = 1; i < intervals.length; ++i) {
-   if (intervals[i][0] < end) {
-    continue;
-   }
-   end = intervals[i][1];
-   cnt++;
-  }
-  return intervals.length - cnt;
- }
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, (o1, o2)->(o1[1]-o2[1]));
+        int cnt = 1;
+        int end = intervals[0][1];
+        for(int i=1; i<n; ++i){
+            int[] interval = intervals[i];
+            if(interval[0]<end){
+                continue;
+            }
+            cnt++;
+            end = interval[1];
+        }
+        return n-cnt;
+    }
 }
-// @lc code=end
-// 注意java lambda表达式的使用
-// 计算能组成的最对的不重叠区间的个数，然后拿总个数做减法，就是需要移除的区间的个数
-// 做选择时使用贪心算法，选择的区间结尾越小，留给后面区间的空间越大，所以先要根据结尾对所有区间进行排序
